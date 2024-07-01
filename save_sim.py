b0aa7cf333e7cf6a3f1dd786043de7fe16e21209
Trey Gower <gowertrey@yahoo.com>
2024-07-01 11:05:59 -0700

    Script for saving taichi simulation data as .npz

---
+++ save_sim.py
@@ -0,0 +1,170 @@
+import os 
+import numpy as np
+import platform
+import json
+import math
+
+"""
+Script: save_sim.py
+Author: Trey Gower
+Date: 2024-07-01
+Description: Saves Taichi simulation and meta data
+"""
+
+""""""
+def save_sim_data(data_designation, x_data, v_data, materials, bounds, sequence_length, DIMENSIONS, time_delta, dx, dt):
+    """Save train.npz, test.npz,or valid.npz to file
+    Args:
+        data_designation: a letter to designate rollout(r), training(t), or validation(v) data
+        x_data: the positional data from a taichi material point method saved in numpy array of shape (ntimestep, nnodes, ndims)
+        v_data: the velocity data from a taichi material point method saved in numpy array of shape (ntimestep, nnodes, ndims)
+        materials: a taichi field defined material = ti.field(dtype=int, shape=n_particles) storing material id data
+    Returns:
+        None
+    """
+    #NOTE:Defining these as global variables in the context of taichi simulations may be necessary
+    #global data_designation
+    #global data
+
+    # Define file_path to save to data, models, rollout folder. Located in directory of this file script
+    system = platform.system().lower()
+
+    if system == 'linux':
+        file_path = "./Flume/dataset"
+    elif system == 'darwin':  # 'Darwin' is the system name for macOS
+        file_path = "Flume/dataset"
+        #file_path = "/Users/" + user_name + "/code-REU/Physics-Informed-ML/dataset/"
+    elif system == 'windows':
+        file_path = "./Flume/dataset"
+    else:
+        file_path = "./Flume/dataset"
+
+    save_relative_to_cwd = True
+    ROLLOUT_PATH="./Flume/rollout"
+    MODEL_PATH="./Flume/models"
+    if save_relative_to_cwd:
+         cwd_path = os.getcwd()
+         file_path = os.path.join(cwd_path, file_path)
+         ROLLOUT_PATH = os.path.join(cwd_path, ROLLOUT_PATH)
+         MODEL_PATH = os.path.join(cwd_path, MODEL_PATH)
+
+    # Ensuring the directories exist within the cwd
+    if not os.path.exists(file_path):
+        os.makedirs(file_path)
+    if not os.path.exists(ROLLOUT_PATH):
+        os.makedirs(ROLLOUT_PATH)
+    if not os.path.exists(MODEL_PATH):
+        os.makedirs(MODEL_PATH)
+    
+    material_id_dict_gns = { "Water": 5, "Sand": 6, "Debris": 0, "Piston": 0, "Boundary": 3} # GNS Mapping Dict from Dr. Kumar
+    material_id_dict_mpm = { "Water": 0, "Snow": 1, "Debris": 2, "Sand": 3, "Piston": 4, "Boundary": 5}
+
+    material_numpy = materials.to_numpy()
+    mat_data_tmp = np.where(material_numpy == material_id_dict_mpm["Water"], material_id_dict_gns["Water"] + (0 * material_numpy), material_numpy)
+
+    mat_data = np.asarray(mat_data_tmp, dtype=object)
+    pos_data = np.stack(x_data, axis=0)
+
+    # Perform downsampling for GNS
+    downsampled_mat_data = mat_data[::100]
+    downsampled_data = pos_data[:,::100,:]
+
+
+    #check version of numpy >= 1.22.0
+    # Newer versions of numpy require the dtype to be explicitly set to object, I think, for some python versions
+    # Should add a check for the python version as well
+
+    if (np.version.version >= '1.22.0'):
+        print("Using numpy version (>= 1.22.0), may require alternative approach to save npz files (e.g. dtype=object): ", np.version.version)
+        pos_data = np.array(np.stack(np.asarray(downsampled_data, dtype=object), axis=0), dtype=object)
+        mat_data = np.asarray(downsampled_mat_data, dtype=object)
+    else:
+        print("Warning: Using numpy version: ", np.version.version)
+        pos_data = np.array(np.stack(downsampled_data, axis=0), dtype=np.float32)
+        mat_data = np.asarray(downsampled_mat_data, dtype=object)
+        # np.array(material_data.tolist()) 
+    
+    print("pos_data: ", pos_data.shape)
+    print("mat_data: ", mat_data.shape)
+    simulation_data = {
+        'simulation_0': (
+            pos_data,
+            mat_data
+        )
+    }
+        
+    if data_designation.lower() in ("r", "rollout", "test"):
+        # Should clarify the difference in naming between test and rollout
+        output_file_path = os.path.join(file_path, "test.npz")
+        np.savez_compressed(f'{file_path}/test.npz', **simulation_data)
+
+    elif data_designation.lower() in ("t", "train"):
+        output_file_path = os.path.join(file_path, "train.npz")
+        np.savez_compressed(f'{file_path}/train.npz', **simulation_data)
+        save_metadata(file_path, v_data, bounds, sequence_length, DIMENSIONS, time_delta, dx, dt)
+
+    elif data_designation.lower() in ("v", "valid"):
+        output_file_path = os.path.join(file_path, "valid.npz")
+        np.savez_compressed(f'{file_path}/valid.npz', **simulation_data)
+        
+    else:
+        output_file_path = os.path.join(cwd_path, "unspecified_sim_data.npz")
+        np.savez_compressed("unspecified_sim_data.npz", **simulation_data)
+        
+    print("Simulation Data Saved to: ", file_path)
+
+def save_metadata(file_path, v_data, bounds, sequence_length, DIMENSIONS, time_delta, dx, dt):
+    """Save metadata.json to file
+    Args:
+        file_path: the path to save the metadata (**Automatically retrieved by system**)
+        v_data: the velocity data from a taichi material point method saved in numpy array of shape (ntimestep, nnodes, ndims)
+        bounds: The boundaries for plotting the animated simulation
+        sequence_length: The number of time steps taken
+        DIMENSIONS: The dimensionality of the simulation (i.e. 2d or 3d)
+        time_delta: Defined as 1 / frames per second of the simulation (typically 20-30 fps)
+        dx: Change in grid sizing defined as the grid_length / # of Grids
+        dt: Time rate of change using CFL for simulation stability (dt = CFL * dx / max_vel)
+
+    ** NOTE: GNS currently does not have use for variables "critical_time_step": dt or "dx": dx. **
+       
+       Returns:
+        None
+    """
+    #Using a list for each time step for formatting
+    #global v_data
+    vel = np.stack(v_data,axis=0)
+    vel_diff = np.diff(vel, axis=0) #computing acceleration along the time dependant axis
+    
+    #Define meta data dictionary from trajectories and timesteps
+    vel_mean = np.nanmean(vel, axis=(0, 1))
+    vel_std = np.nanstd(vel, axis=(0, 1)) #standard deviation of velocity
+    acc_mean = np.nanmean(vel_diff, axis=(0, 1)) #mean acceleration from velocity
+    acc_std = np.nanstd(vel_diff, axis=(0, 1))  #standard deviation of acceleration from velocity 
+   
+    # Convert numpy types to native Python types
+    vel_mean = [float(x) for x in vel_mean]
+    vel_std = [float(x) for x in vel_std]
+    acc_mean = [float(x) for x in acc_mean]
+    acc_std = [float(x) for x in acc_std]
+    
+    #Formatting enforced
+    # Might want to replace 0.0025 with the dt actually used, etc., but maybe its like this for a reason
+    metadata = {
+        "bounds": bounds,
+        "sequence_length": sequence_length, 
+        "default_connectivity_radius": 0.5, 
+        "dim": DIMENSIONS, 
+        "dt": time_delta, 
+        "dx": dx,
+        "critical_time_step": dt,
+        "vel_mean": vel_mean, #[5.123277536458455e-06, -0.0009965205918140803], 
+        "vel_std": vel_std, #[0.0021978993231675805, 0.0026653552458701774], 
+        "acc_mean": acc_mean, #[5.237611158734309e-07, 2.3633027988858656e-07], 
+        "acc_std": acc_std, #[0.0002582944917306106, 0.00029554531667679154]
+    }
+    
+    
+    # Write metadata to a JSON file
+    with open(os.path.join(file_path, 'metadata.json'), 'w') as file:
+        json.dump(metadata, file)
+        print("Metadata Saved!\n")
