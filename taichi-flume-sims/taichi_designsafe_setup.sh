391e0689534a61dc132f9035b29eb96865db9a6d
JustinBonus <bonus@berkeley.edu>
2024-07-20 21:31:26 -0700

    Update setup for repo and remote workflow for frontera/lonestar6

---
+++ taichi-flume-sims/taichi_designsafe_setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+which python3
+python3 -m pip install --upgrade pip
+python3 -m pip install taichi==1.7.1
+python3 -m pip install imageio
+python3 -m pip install ffmpeg
+
+
+python3 -m pip list
+
+# Assuming that mpm99_designsafe.py is in the same directory as this script
+mkdir taichi-output
+mkdir taichi-output/frames
+export TAICHI_SCRIPT="mpm99_designsafe.py"
+export INPUT_DIR="."
+export OUTPUT_DIR="./taichi-output/"
+python3 ${INPUT_DIR}/${TAICHI_SCRIPT}
+
